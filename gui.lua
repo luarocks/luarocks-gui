9697ba4edd8e3c74fce26386e82e7b9c1bf57321
Thijs Schreijer <thijs@thijsschreijer.nl>
2018-07-21 01:18:11 +0200

    automatically open the webbrowser after starting the gui server

--- gui.lua
+++ gui.lua
@@ -11,16 +11,21 @@ You can view the gui by visting: http://localhost:8080/ from your web browser.
 
 function gui.command()
 
+    local port = 8080
+
     -- Copas needs to patch "socket.http" to use the copas sockets. Hence
     -- it requires `socket.http` to not have been loaded. So let's clear it.
     -- (normally not necessary, but now LuaRocks already loaded LuaSocket)
     package.loaded["socket.http"] = nil
 
+    local fs = require "luarocks.fs"
+    local copas = require "copas"
     local datafile = require "datafile"
     local xavante = require "xavante"
     local redirect = require "xavante.redirecthandler"
     local filehandler = require "xavante.filehandler"
     local cgiluahandler = require "xavante.cgiluahandler"
+
 
     -- Define here where Xavante HTTP documents scripts are located
     local path = datafile.path("pages/index.lp", "r")
@@ -46,13 +51,26 @@ function gui.command()
         },
     }
 
+
+    -- create the webserver
     xavante.HTTP{
-        server = {host = "*", port = 8080},
+        server = {host = "*", port = port},
 
         defaultHost = {
           rules = simplerules
         }
     }
+
+    -- schedule a task to open the webbrowser
+    copas.addthread(function()
+        copas.sleep(0.1)  -- wait for a tiny bit to allow Xavante to start
+        print("Serving the LuaRocks gui on 'http://localhost:" .. port .. "'.")
+        local ok = fs.browser("http://localhost:" .. port)
+        if not ok then
+          print("WARNING: Failed to open the browser, please visit the url manually.")
+        end
+    end)
+
     xavante.start()
 end
 
